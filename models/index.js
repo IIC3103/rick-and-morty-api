e00b5193b3ba69e702b0cf6c1d7fac843af472f1
arturokunder <arturokunder@users.noreply.github.com>
2020-04-05 18:14:10 -0400

    Update episodes urls with API_URL

--- models/index.js
+++ models/index.js
@@ -1,5 +1,31 @@
+var character = require('./Character'),
+location = require('./Location'),
+episode = require('./Episode');
+
+
+replace_url =  function(url) {
+  return url.replace('https://rickandmortyapi.com/api', process.env.API_URL)
+};
+
+// Replace objects url with API_URL const
+if(process.env.API_URL && process.env.API_URL !== 'https://rickandmortyapi.com/api') {  
+  episode.find().forEach(function(ep) {
+    updated_characters = [];
+    for(var i = 0; i < ep.characters.length; i++) {
+      updated_characters.append(replace_url(ep.characters[i]));
+    }
+
+    episode.update({_id: ep._id}, {
+      $set: {
+        'url': replace_url(ep.url),
+        'characters' : updated_characters
+      }
+    });
+  });
+}
+
 module.exports = {
-  character: require('./Character'),
-  location: require('./Location'),
-  episode: require('./Episode')
+  character: character,
+  location: location,
+  episode: episode
 }
